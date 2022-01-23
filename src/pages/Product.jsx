acd4187ec412d9c13ff19995929f324be60ef912
bhasi <anilbhasib@gmail.com>
2022-01-23 10:45:32 +0530

    desc text styled

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -10,6 +10,7 @@ const data = {
 function Product() {
   return (
     <div className="procut-container">
+      {/* main product display */}
       <div className="hero-product">
         <img
           src="https://cdn.shopify.com/s/files/1/0571/4903/0572/products/eeno-product-400px_550x825.jpg"
@@ -22,13 +23,16 @@ function Product() {
         </span>
       </div>
       <div className="desc-product">
+        {/* badge and table section */}
         <div className="table-badge">
+          {/* 1)badge section */}
           <div className="badge">
             <img
               src="https://lostlochspirits.com/wp-content/uploads/2021/05/eenoo-page-awards-2021-update-300x300-1.png"
               alt=""
             />
           </div>
+          {/* 2)table-section */}
           <div className="table">
             <table>
               <tr>
@@ -50,28 +54,41 @@ function Product() {
             </table>
           </div>
         </div>
+        {/* description section */}
         <div className="description">
-          eeNoo, From the old Scots tongue; eenoo – just now, at the present
-          time, in a short time, at once. Scottish gin is at the forefront of a
-          recent gin renaissance and so we thought eeNoo a fine and fitting name
-          for our gin, as the present time is the heyday of gin production in
-          Scotland. When we crafted our gin, eeNoo, we wanted to create a
-          flavour profile that encompassed the essence of a traditional gin
-          whilst embracing flavours of the Scottish Highlands. eeNoo is
-          distilled with Royal Deeside honey. The flavour profile of our honey
-          is unique to the local area and is made up of heather, willow herb and
-          clover pollen. To complement the honey we add heather flowers and
-          other local botanicals. We forage a percentage of our brambleberries,
-          raspberries and rosehip from the Deeside area and source the remainder
-          from berry farms in Aberdeenshire and Angus. As Scottish juniper is in
-          a critical state of decline, we source the finest juniper from the
-          Italian highlands. We then add coriander seeds, angelica root,
-          liquorice root, orange and lemon peel to give our gin a traditional
-          yet smooth fruity profile. Water from an ancient spring in the
-          Cairngorms National Park is added to the mix. The water is some of the
-          purest in the world and spends 50 years being gently filtered through
-          layers of underground rocks and crevices before becoming part of our
-          gin.
+          <p className="main-heading">
+            eeNoo, From the old Scots tongue; eenoo – just now, at the present
+            time, in a short time, at once.
+          </p>
+          <p className="sub-heading">
+            Scottish gin is at the forefront of a recent gin renaissance and so
+            we thought eeNoo a fine and fitting name for our gin, as the present
+            time is the heyday of gin production in Scotland.
+          </p>
+          <p className="content">
+            <p>
+              When we crafted our gin, eeNoo, we wanted to create a flavour
+              profile that encompassed the essence of a traditional gin whilst
+              embracing flavours of the Scottish Highlands.
+            </p>{" "}
+            <p>
+              eeNoo is distilled with Royal Deeside honey. The flavour profile
+              of our honey is unique to the local area and is made up of
+              heather, willow herb and clover pollen. To complement the honey we
+              add heather flowers and other local botanicals. We forage a
+              percentage of our brambleberries, raspberries and rosehip from the
+              Deeside area and source the remainder from berry farms in
+              Aberdeenshire and Angus. As Scottish juniper is in a critical
+              state of decline, we source the finest juniper from the Italian
+              highlands. We then add coriander seeds, angelica root, liquorice
+              root, orange and lemon peel to give our gin a traditional yet
+              smooth fruity profile.
+            </p>{" "}
+            Water from an ancient spring in the Cairngorms National Park is
+            added to the mix. The water is some of the purest in the world and
+            spends 50 years being gently filtered through layers of underground
+            rocks and crevices before becoming part of our gin.
+          </p>
         </div>
       </div>
     </div>
